d8d32ae70c9fbf9b35b334da6c506ffd92e1787b
Daniel Malede <danielmalede@gmail.com>
2024-11-14 08:25:18 +0200

    first commit

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,16 @@
 public class TimeCalc {
     public static void main(String[] args) {
         // Replace this comment with your code
+        int hours = (int) Integer.parseInt("" + args[0].charAt(0) + "" + args[0].charAt(1));
+        int minute = (int) Integer.parseInt("" + args[0].charAt(3) + "" + args[0].charAt(4));
+        int minutesAdd = (int) Integer.parseInt(args[1]);
+        int totalTime = (hours * 60) + minute + minutesAdd;
+        int dividedHour = totalTime / 60;
+        int moduleDividedHour = hours <=12 ? dividedHour % 24 : hours;
+        // variable for minutes 
+        int newMinutes = totalTime - (dividedHour * 60);
+        System.out.println( moduleDividedHour+ ":" +newMinutes);
+        // System.out.println(hours * 60 + " " + minute + " " +minutesAdd);
+
     }
 }
