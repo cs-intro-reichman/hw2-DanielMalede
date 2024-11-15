0511ca3578eba0278b486adf7a394cd371f4c49c
Daniel Malede <danielmalede@gmail.com>
2024-11-15 18:44:31 +0200

    add loop  for str prints

--- Cheers.java
+++ Cheers.java
@@ -3,23 +3,26 @@ public class Cheers {
         public static void main(String[] args) {
                 // Replace this comment with your code
                 String str = args[0];
+                int roundsPrints = Integer.parseInt(args[1]);
                 char[] checkChars = { 'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X' };
                 for (int i = 0; i < str.length(); i++) {
-                        Boolean bool= true;
+                        Boolean bool = true;
 
                         for (int j = 0; j < checkChars.length; j++) {
+
                                 if (str.indexOf(checkChars[j], i) == i) {
                                         System.out.println(
                                                         "Give me an " + str.charAt(i) + ": " + str.charAt(i) + "!");
-                                                        bool = false;
-                                                }
+                                        bool = false;
+                                }
                         }
-                        if (bool != false) {
+                        if (bool == true) {
                                 System.out.println("Give me a " + str.charAt(i) + ": " + str.charAt(i) + "!");
                         }
-
-
-
+                }
+                System.out.println("What does that spell?");
+                for (int i = 0; i < roundsPrints; i++) {
+                        System.out.println(str);
                 }
         }
 }
