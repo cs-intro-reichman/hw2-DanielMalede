4e0ad46d2cc7455be9915f51a44e9a49f490ac81
Daniel Malede <danielmalede@gmail.com>
2024-11-15 14:53:22 +0200

    calculate

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,16 +1,19 @@
 public class TimeCalc {
     public static void main(String[] args) {
         // Replace this comment with your code
-        int hours = (int) Integer.parseInt("" + args[0].charAt(0) + "" + args[0].charAt(1));
-        int minute = (int) Integer.parseInt("" + args[0].charAt(3) + "" + args[0].charAt(4));
-        int minutesAdd = (int) Integer.parseInt(args[1]);
+        int hours = Integer.parseInt("" + args[0].charAt(0) + "" + args[0].charAt(1));
+        int minute = Integer.parseInt("" + args[0].charAt(3) + "" + args[0].charAt(4));
+        int minutesAdd = Integer.parseInt(args[1]);
+        // calculate hours and minutes
         int totalTime = (hours * 60) + minute + minutesAdd;
         int dividedHour = totalTime / 60;
-        int moduleDividedHour = hours <=12 ? dividedHour % 24 : hours;
-        // variable for minutes 
+        int moduleDividedHour = dividedHour % 24;
         int newMinutes = totalTime - (dividedHour * 60);
-        System.out.println( moduleDividedHour+ ":" +newMinutes);
-        // System.out.println(hours * 60 + " " + minute + " " +minutesAdd);
+        // Final variable for minutes and Hours
+        String minuteString = newMinutes < 10 ? "0" + newMinutes : "" + newMinutes;
+        String hourString = moduleDividedHour < 10 ? "0" + moduleDividedHour : "" + moduleDividedHour;
+
+        System.out.println(hourString + ":" + minuteString);
 
     }
 }
