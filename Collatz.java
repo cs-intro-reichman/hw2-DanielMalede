6afdb01413a0964d041ae493495ae81fd86cc17d
Daniel Malede <danielmalede@gmail.com>
2024-11-16 23:15:06 +0200

    add "v" and "c" if

--- Collatz.java
+++ Collatz.java
@@ -6,41 +6,45 @@ public class Collatz {
 		int roundCounter = 1;
 		int counter = 0;
 
-		for (int i = 0; i < number; i++) {
-			System.out.print(i + 1 + " ");
-			number = i + 1;
-			while (counter < 3) {
-				if (number % 2 == 0) {
-					number = number / 2;
-					System.out.print(number + " ");
+		if (args[1] == "v") {
+			for (int i = 0; i < number; i++) {
+				System.out.print(i + 1 + " ");
+				number = i + 1;
+				while (counter < 3) {
+					if (number % 2 == 0) {
+						number = number / 2;
+						System.out.print(number + " ");
 
-				} else {
-					number = (3 * number) + 1;
-					System.out.print(number + " ");
+					} else {
+						number = (3 * number) + 1;
+						System.out.print(number + " ");
+					}
+					roundCounter += 1;
+					counter++;
+
 				}
-				roundCounter += 1;
-				counter++;
+				while (number != 1) {
+					if (number % 2 == 0) {
+						number = number / 2;
+						System.out.print(number + " ");
 
-			}
-			while (number != 1) {
-				if (number % 2 == 0) {
-					number = number / 2;
-					System.out.print(number + " ");
+					} else {
+						number = (3 * number) + 1;
+						System.out.print(number + " ");
+					}
+					roundCounter += 1;
 
-				} else {
-					number = (3 * number) + 1;
-					System.out.print(number + " ");
 				}
-				roundCounter += 1;
 
+				System.out.print("(" + roundCounter + ")");
+				number = Integer.parseInt(args[0]);
+				roundCounter = 1;
+				System.out.println("");
 			}
-
-			System.out.print("(" + roundCounter + ")");
-			number = Integer.parseInt(args[0]);
-			roundCounter = 1;
-			System.out.println("");
+			System.out.println("Every one of the first " + args[0] + " hailstone sequences reached 1.");
 		}
 		System.out.println("Every one of the first " + args[0] + " hailstone sequences reached 1.");
+
 
 	}
 }
