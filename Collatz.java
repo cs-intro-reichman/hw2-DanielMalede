059618e5305a00c6610c94353b86c875b2dd5a6a
Daniel Malede <danielmalede@gmail.com>
2024-11-16 23:47:57 +0200

    change valueOf to indexOf

--- Collatz.java
+++ Collatz.java
@@ -7,7 +7,7 @@ public class Collatz {
 		int roundCounter = 1;
 		int counter = 0;
 
-		if (checkIfVOrCString.valueOf("v") == "v") {
+		if (checkIfVOrCString.indexOf("v") == 0) {
 			for (int i = 0; i < number; i++) {
 				System.out.print(i + 1 + " ");
 				number = i + 1;
@@ -34,16 +34,15 @@ public class Collatz {
 						System.out.print(number + " ");
 					}
 					roundCounter += 1;
-
 				}
-
 				System.out.print("(" + roundCounter + ")");
 				number = Integer.parseInt(args[0]);
 				roundCounter = 1;
 				System.out.println("");
 			}
 			System.out.println("Every one of the first " + args[0] + " hailstone sequences reached 1.");
-		} else {
+		} 
+		else {
 			System.out.println("Every one of the first " + args[0] + " hailstone sequences reached 1.");
 		}
 	}
